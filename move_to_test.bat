4ce7e2e8c06a7a7e5bdb1db93c33509345446d5f
Olle Karlson Reutercrona <ollekreutercrona@gmail.com>
2024-08-21 15:22:16 +0200

    Did a thing

---
+++ move_to_test.bat
@@ -0,0 +1,5 @@
+mkdir Source\UnitTest\Source\Client
+mkdir Source\UnitTest\Source\Server
+copy Source\Game\source\*.* Source\UnitTest\Source\Client
+copy Source\Server\Source\Server\*.* Source\UnitTest\Source\Server
+pause
